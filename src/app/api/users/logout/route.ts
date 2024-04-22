336cb4130a912bc55801ec28cdf4ebdef7f9e6ee
ChandraShekhar <csyadav0513@gmail.com>
2024-04-22 21:12:30 +0530

    Implement logout route

---
+++ src/app/api/users/logout/route.ts
@@ -0,0 +1,26 @@
+import { dbConnect } from "@/db/dbConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+
+dbConnect();
+
+export async function GET(request: NextRequest) {
+  try {
+    const response = NextResponse.json({
+      message: "Logged Out Successfully 🤩👍",
+      success: true,
+    });
+
+    response.cookies.set("token", "", {
+      httpOnly: true,
+      expires: new Date(0),
+    });
+
+    return response;
+
+
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
